39b7094f8572c60ab3fbf1b9498f5dd33b053102
W-Sikora <242612@student.pwr.edu.pl>
2022-12-05 17:15:11 +0100

    dodano PlannedTransactionCommand;

---
+++ src/main/java/pl/wsikora/successbudget/plannedtransaction/application/command/PlannedTransactionCommand.java
@@ -0,0 +1,11 @@
+package pl.wsikora.successbudget.plannedtransaction.application.command;
+
+import pl.wsikora.successbudget.plannedtransaction.domain.PlannedTransaction;
+
+
+public interface PlannedTransactionCommand {
+
+    void save(PlannedTransaction plannedTransaction);
+
+    void delete(Long id);
+}
